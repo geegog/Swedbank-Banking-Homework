d8ab6c41c474a8feebefa15c64daa4379e4f2937
geegog <gee_gog@yahoo.com>
2026-06-24 12:42:46 +0300

    - get Transactions function

--- src/main/java/com/swedbank/transaction/application/service/TransactionService.java
+++ src/main/java/com/swedbank/transaction/application/service/TransactionService.java
@@ -1,17 +1,25 @@
 package com.swedbank.transaction.application.service;
 
 import com.swedbank.common.domian.Money;
+import com.swedbank.transaction.application.dto.TransactionDto;
 import com.swedbank.transaction.application.dto.TransactionRequest;
 import com.swedbank.transaction.domian.model.Transaction;
 import com.swedbank.transaction.domian.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+
+    private final ModelMapper modelMapper;
 
     private void saveTransaction(Transaction transaction) {
         transactionRepository.save(transaction);
@@ -26,6 +34,13 @@ public class TransactionService {
             transaction.setReference(transactionRequest.getReference());
         }
         transaction.setValue(Money.of(transactionRequest.getValue().getAmount(), transactionRequest.getValue().getCurrency()));
+    }
+
+    public List<TransactionDto> getTransactions(String accountNumber, UUID userId) {
+
+        var transactions = transactionRepository.findByAccountNumberAndUserId(accountNumber, userId);
+
+        return modelMapper.map(transactions, new TypeToken<List<TransactionDto>>() {}.getType());
     }
 
 }
